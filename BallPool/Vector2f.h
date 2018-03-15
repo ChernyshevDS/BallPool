c5fd81604e834a213ae3eed1fbc49d37b4c35517
ChernyshevDS <chernyshev_ds@mail.ru>
2018-03-15 23:36:17 +0300

    Added tests for Vector2f

--- BallPool/Vector2f.h
+++ BallPool/Vector2f.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cmath>
-
+#include <numeric>
 // —труктура, моделирующа€ двумерный вектор
 struct Vector2f
 {
@@ -19,6 +19,16 @@ struct Vector2f
 		return std::hypot(x, y);
 	}
 
+	void normalize()
+	{
+		float len = length();
+		if (len != 0) 
+		{
+			x /= len;
+			y /= len;
+		}
+	}
+
 	Vector2f operator+(const Vector2f& other) const
 	{
 		return { x + other.x, y + other.y };
@@ -28,6 +38,47 @@ struct Vector2f
 	{
 		x += other.x;
 		y += other.y;
+		return *this;
+	}
+
+	Vector2f operator-(const Vector2f& other) const
+	{
+		return { x - other.x, y - other.y };
+	}
+
+	Vector2f& operator-=(const Vector2f& other)
+	{
+		x -= other.x;
+		y -= other.y;
+		return *this;
+	}
+
+	Vector2f operator-() const
+	{
+		return { -x, -y };
+	}
+
+	bool operator==(const Vector2f &a) const
+	{
+		return ((a.x == this->x) && (a.y == this->y));
+	}
+
+	bool operator!=(const Vector2f &a) const
+	{
+		return !(a == *this);
+	}
+
+	Vector2f& operator*=(float scale)
+	{
+		this->x *= scale;
+		this->y *= scale;
+		return *this;
+	}
+
+	Vector2f& operator/=(float scale)
+	{
+		this->x /= scale;
+		this->y /= scale;
 		return *this;
 	}
 };
@@ -40,4 +91,33 @@ inline Vector2f operator*(const Vector2f& v, float scale)
 inline Vector2f operator*(float scale, const Vector2f& v)
 {
 	return v * scale;
+}
+
+inline Vector2f operator/(const Vector2f& v, float scale)
+{
+	return { v.x / scale, v.y / scale };
+}
+
+inline float dot(const Vector2f& a, const Vector2f& b)
+{
+	return a.x * b.x + a.y * b.y;
+}
+
+inline Vector2f normed(const Vector2f& a)
+{
+	float len = a.length();
+	if (len == 0)
+		return Vector2f{};
+	return a / len;
+}
+
+inline Vector2f perp(const Vector2f& a)
+{
+	return Vector2f{ -a.y, a.x };
+}
+
+inline float dist(const Vector2f& a, const Vector2f& b)
+{
+	const Vector2f c = b - a;
+	return c.length();
 }
